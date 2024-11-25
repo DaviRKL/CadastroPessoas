9ca18a171cc0c85368105e1fd04ebdb87a27f23f
DaviRKL <davirkl07@gmail.com>
2024-11-25 14:50:44 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroPessoas.Models
+{
+    public class Cliente
+    {
+        [Key]
+
+        public int id { get; set; }
+        [Required]
+
+        public string nome { get; set; }
+    }
+}
